8626df55d9d0c1fc5e3cb0f33280ac912f8ae3f4
Nikhil <nikhil.patel.np@outlook.com>
2021-10-12 14:42:53 +0100

    replies error

--- tweet_hunter/pages/api/data.js
+++ tweet_hunter/pages/api/data.js
@@ -3,7 +3,10 @@
 export default async function handler(req, res) {
   const { username, count, retweets, replies } = req.query
 
-  const endpoint = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${username}&include_rts=${retweets}&count=${count}&exclude_replies=${!replies}`
+  console.log('RT:' + retweets)
+  console.log(replies)
+
+  const endpoint = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${username}&include_rts=${retweets==='true'}&count=${count}&exclude_replies=${replies!=='true'}`
   const twitterRes = await fetch(endpoint, {
     headers: {'Authorization': "Bearer " + process.env.TWITTER_TOKEN}
   });
